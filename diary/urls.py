07e01c34a19c89cc328a73122f4ed1d16d4f9f1c
肖子涵 <762297355@qq.com>
2024-05-05 01:51:32 +0800

    完成写日记的功能（但是当前用户这里还没改

--- diary/urls.py
+++ diary/urls.py
@@ -6,4 +6,6 @@ app_name = "diary"
 urlpatterns = [
     path("", views.diary, name="diary"),
     path("<int:diary_id>/", views.detail, name="detail"),
+    path('add_diary/', views.add_diary, name='add_diary'),
+    path('search_location/', views.search_location, name='search_location'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.templatetags.static import static
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import redirect
+from django.http import JsonResponse
 
+from travel.models import Destination
+from account.models import User
 from .models import Diary
 def diary(request):
     ## 修改成自己写的排序
@@ -20,3 +25,25 @@ def detail(request, diary_id):
     }
     return render(request, 'diary/detail.html', context)
 
+@require_http_methods(["POST"])
+def add_diary(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        location_id = request.POST.get('location_id')
+        location = get_object_or_404(Destination, pk=location_id)
+        author = get_object_or_404(User, username='小明')
+        new_diary = Diary(author=author, title=title, content=content, location=location)
+        # new_diary = Diary(author=request.user, title=title, content=content, location=location)
+        new_diary.save()
+        return redirect('/diary/')  # Redirect to a new URL after saving
+
+# 在日记添加表单中处理搜索游学地
+def search_location(request):
+    query = request.GET.get('query', '')
+    if query:
+        destinations = Destination.objects.filter(name__icontains=query)
+        results = [{'id': dest.id, 'name': dest.name} for dest in destinations]
+    else:
+        results = []
+    return JsonResponse(results, safe=False)
