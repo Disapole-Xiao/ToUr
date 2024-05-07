fb523b27b93eae3c694a0d97cf9afc3f4625769e
faier-3 <79971327@qq.com>
2024-05-07 21:21:24 +0800

    更新了1.json

--- tool/newnumber.py
+++ tool/newnumber.py
@@ -1,5 +1,6 @@
 import json
 
+# 定义一个函数用于更新节点和连接信息的ID
 def update_ids(data, old_id, new_id):
     # 更新节点ID
     for node in data['nodes']:
@@ -12,16 +13,49 @@ def update_ids(data, old_id, new_id):
             if adj_node['id'] == old_id:
                 adj_node['id'] = new_id
 
+def update_amenities_ids(data):
+    new_id = 0
+    for amenity in data['amenities']:
+        amenity['id'] = new_id
+        new_id += 1
+
+def update_restaurants_ids(data):
+    new_id = 0
+    for restaurant in data['restaurants']:
+        restaurant['id'] = new_id
+        new_id += 1
+
+def update_attractions_ids(data):
+    new_id = 0
+    for attraction in data['attractions']:
+        attraction['id'] = new_id
+        new_id += 1
+
 # 读取JSON文件
-with open('data/maps/1.json', 'r') as file:
+with open('static/maps/1.json', 'r',encoding="utf-8") as file:
     data = json.load(file)
+
+# 创建一个字典，用于存储旧ID和新ID之间的映射关系
+id_mapping = {}
 
 # 从0开始编号并更新相关连接信息中的ID
 for idx, node in enumerate(data['nodes']):
     old_id = node['id']
     new_id = str(idx)
+    id_mapping[old_id] = new_id
     update_ids(data, old_id, new_id)
 
-# 写回JSON文件
-with open('data/maps/1.json', 'w') as file:
-    json.dump(data, file, indent=4)
+# 更新attractions中的entr_point中的数字
+for attraction in data['attractions']:
+    old_point = attraction['entr_point'][0]
+    new_point = id_mapping.get(str(old_point), None)
+    if new_point is not None:
+        attraction['entr_point'][0] = int(new_point)
+
+update_amenities_ids(data)
+update_restaurants_ids(data)
+update_attractions_ids(data)
+
+# 将更新后的数据写回JSON文件
+with open('static/maps/1.json', 'w',encoding="utf-8") as file:
+    json.dump(data, file,ensure_ascii=False, indent=4)
