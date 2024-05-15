973f54f5871e95b72b4ed943b7ab9be6cb000b67
faier-3 <79971327@qq.com>
2024-05-15 11:30:50 +0800

    修复了dist的算法问题

--- src/djst.py
+++ src/djst.py
@@ -1,6 +1,7 @@
 import json
 from map import Map
-inf = float('inf')
+
+inf = float("inf")
 
 
 def adjlist_distance(nodes):
@@ -15,6 +16,7 @@ def adjlist_distance(nodes):
 
     return adj_list
 
+
 def adjlist_time(nodes):
     adj_list = [[] for _ in range(len(nodes))]
 
@@ -22,57 +24,63 @@ def adjlist_time(nodes):
         adjacencies = node["adj"]
         for adj in adjacencies:
             neighbor = adj["node"]
-            distance=adj["distance"]
-            congestion=adj["congestion"]
-            weight = distance/congestion
+            distance = adj["distance"]
+            congestion = adj["congestion"]
+            weight = distance / congestion
             adj_list[node["id"]].append((neighbor, weight))
 
         del node["adj"]
 
     return adj_list
 
+
 # 返回值是id序列
 def route_sgl(cur_map: Map, start: int, end: int, mode: str) -> list:
     # 从 Map 中获取边存储到邻接表 adj_list 中
-    if mode == 'distance':
-        adj_list=adjlist_distance(cur_map.nodes)
-    elif mode == 'time' :
-        adj_list=adjlist_time(cur_map.nodes)
-    
+    if mode == "distance":
+        adj_list = adjlist_distance(cur_map.nodes)
+    elif mode == "time":
+        adj_list = adjlist_time(cur_map.nodes)
+
     # dijkstra
     M = len(adj_list)
     dist = [inf] * M
     visit = [1] * M
     path = [-1] * M
     dist[start] = 0
+    visit[start] = 0
     temp = start
+
     while start != end:
         Min = inf
-        Next = -1
         for neighbor, weight in adj_list[start]:
-            if visit[start]  and dist[start] + weight < dist[neighbor]:
+            if  dist[start] + weight < dist[neighbor]:
                 dist[neighbor] = dist[start] + weight
                 path[neighbor] = start
-            if visit[start] and dist[neighbor] < Min:
-                Min = dist[neighbor]
-                Next = neighbor
+        for i in range(0,M):
+            if visit[i] and dist[i] < Min:
+                Min = dist[i]
+                Next = i
         if Min == inf:
+            print("No")
             break
         start = Next
         visit[start] = 0
 
     # 获取最短路径
     shortestPath = []
-    print('visit:', [x[0] for x in enumerate(visit) if x[1] == 0])
-    print('path:', {x[0]:x[1] for x in enumerate(path) if x[1] != -1})
+    # print("visit:", [x[0] for x in enumerate(visit) if x[1] == 0])
+    # print("path:", {x[0]: x[1] for x in enumerate(path) if x[1] != -1})
+    shortestPath.append(end)
     while end != temp:
         shortestPath.append(path[end])
         end = path[end]
     shortestPath.reverse()
     return shortestPath
 
+
 if __name__ == "__main__":
-    with open("static/maps/1.json", "r", encoding='utf-8') as f:
+    with open("static/maps/1.json", "r", encoding="utf-8") as f:
         jsonstr = f.read()
     map = Map(jsonstr)
-    print(route_sgl(map, 0, 1, 'distance'))
+    print(route_sgl(map,162, 316,"distance"))
